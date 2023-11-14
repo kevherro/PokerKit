b38d93156367a6ea7c82fa3592b3ff0dd01571d1
Kevin Herro <kvherro@gmail.com>
2023-11-14 13:56:11 -0800

    hand evaluators scaffolding

---
+++ Sources/Evaluators/HandEvaluators/FlushEvaluator.swift
@@ -0,0 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the PokerKit project.
+//
+// Copyright (c) 2023 Kevin Herro
+//
+//===----------------------------------------------------------------------===//
+
+import Types
+
+struct FlushEvaluator {}
---
+++ Tests/EvaluatorTests/PairEvaluatorTests.swift
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the PokerKit project.
+//
+// Copyright (c) 2023 Kevin Herro
+//
+//===----------------------------------------------------------------------===//
+
+import _PokerKitTestSupport
+
+@testable import Evaluators
+
+final class PairEvaluatorTests: PokerKitTestCase {
+  private let sut = PairEvaluator()
+}
