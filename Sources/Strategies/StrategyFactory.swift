b8f22e9eaecf92ec6a0f00572a1941f8a4d3e133
Kevin Herro <kvherro@gmail.com>
2023-11-14 12:12:11 -0800

    strategy factory scaffolding

---
+++ Sources/Strategies/StrategyFactory.swift
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the PokerKit project.
+//
+// Copyright (c) 2023 Kevin Herro
+//
+//===----------------------------------------------------------------------===//
+
+protocol StrategyProtocol {}
+
+struct StrategyFactory {
+  func makeStrategy(_ strategy: Strategy) -> StrategyProtocol {
+    switch strategy {
+    default:
+      fatalError()
+    }
+  }
+}
