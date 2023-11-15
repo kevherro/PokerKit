a7727809bcb3de82a7185a4f4bd1fc094f002a1b
Kevin Herro <kvherro@gmail.com>
2023-11-15 09:18:58 -0800

    evaluators: hasTopPair

--- Sources/Evaluators/HandEvaluators/PairEvaluator.swift
+++ Sources/Evaluators/HandEvaluators/PairEvaluator.swift
@@ -8,15 +8,34 @@
 
 import Types
 
-struct PairEvaluator {
+/// `PairEvaluator` provides methods to evaluate specific paired hand types.
+/// Each method independently checks for a particular hand type without considering
+/// the overall strength of the hand in the context of NLHE poker hand rankings.
+///
+/// - Note: `PairEvaluator` does not rank hand strengths.
+/// Therefore, it's possible for multiple methods to return `true` for the same hand.
+/// For example, a hand could satisfy both `hasTopPair` and `hasTrips`.
+public struct PairEvaluator {
 
-  // MARK: Top Pair
-
-  func hasTopPair(
-    cards: [Card],
+  /// A hand of one pair, using the highest card on the board,
+  /// regardless of how many times that card appears on the board.
+  ///
+  /// - Parameters:
+  ///   - holeCards: The player's hole cards.
+  ///   - communityCards: The community cards.
+  ///
+  /// - Returns: True if the player has top pair, false otherwise.
+  public func hasTopPair(
+    holeCards: [Card],
     communityCards: [Card]
   ) -> Bool {
-    return false
+    guard let highestRankInCommunityCards = highestRank(in: communityCards) else { return false }
+    return holeCards.contains { $0.rank == highestRankInCommunityCards }
+  }
+
+  private func highestRank(in cards: [Card]) -> Rank? {
+    let sortedRanks = cards.map(\.rank).sorted(by: >)
+    return sortedRanks.first
   }
 
   // MARK: Top Pair Top Kicker
--- Tests/EvaluatorTests/PairEvaluatorTests.swift
+++ Tests/EvaluatorTests/PairEvaluatorTests.swift
@@ -12,4 +12,99 @@ import _PokerKitTestSupport
 
 final class PairEvaluatorTests: PokerKitTestCase {
   private let sut = PairEvaluator()
+
+  // MARK: Top Pair
+
+  func test_top_pair() {
+    let holeCards = [card(.ace, .hearts), card(.king, .hearts)]
+    let communityCards = [
+      card(.ace, .diamonds),
+      card(.five, .hearts),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasTopPair)
+  }
+
+  func test_top_pair_quads() {
+    let holeCards = [card(.ace, .hearts), card(.ace, .diamonds)]
+    let communityCards = [
+      card(.ace, .clubs),
+      card(.ace, .spades),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasTopPair)
+  }
+
+  func test_top_pair_set() {
+    let holeCards = [card(.ace, .hearts), card(.ace, .diamonds)]
+    let communityCards = [
+      card(.ace, .clubs),
+      card(.king, .spades),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasTopPair)
+  }
+
+  func test_top_pair_trips() {
+    let holeCards = [card(.ace, .hearts), card(.king, .diamonds)]
+    let communityCards = [
+      card(.ace, .clubs),
+      card(.ace, .spades),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasTopPair)
+  }
+
+  func test_no_top_pair_too_few_holeCards() {
+    let hasTopPair = sut.hasTopPair(holeCards: [], communityCards: [])
+    expectFalse(hasTopPair)
+  }
+
+  func test_no_top_pair_no_pair() {
+    let holeCards = [card(.king, .hearts), card(.ten, .hearts)]
+    let communityCards = [
+      card(.ace, .diamonds),
+      card(.five, .hearts),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectFalse(hasTopPair)
+  }
+
+  func test_no_top_pair_too_low() {
+    let holeCards = [card(.king, .diamonds), card(.ten, .hearts)]
+    let communityCards = [
+      card(.ace, .diamonds),
+      card(.king, .hearts),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectFalse(hasTopPair)
+  }
+
+  func test_no_top_pair_paired_board() {
+    let holeCards = [card(.king, .hearts), card(.ten, .hearts)]
+    let communityCards = [
+      card(.ace, .diamonds),
+      card(.ace, .hearts),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectFalse(hasTopPair)
+  }
+
+  func test_no_top_pair_pocket_pair() {
+    let holeCards = [card(.ace, .hearts), card(.ace, .diamonds)]
+    let communityCards = [
+      card(.king, .diamonds),
+      card(.queen, .hearts),
+      card(.two, .clubs),
+    ]
+    let hasTopPair = sut.hasTopPair(holeCards: holeCards, communityCards: communityCards)
+    expectFalse(hasTopPair)
+  }
 }
