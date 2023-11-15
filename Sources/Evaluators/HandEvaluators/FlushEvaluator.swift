7d5779e404f17379ec017c7f451b5ba8a2b00de3
Kevin Herro <kvherro@gmail.com>
2023-11-15 11:46:01 -0800

    evaluators: hasFlush

--- Sources/Evaluators/HandEvaluators/FlushEvaluator.swift
+++ Sources/Evaluators/HandEvaluators/FlushEvaluator.swift
@@ -8,4 +8,37 @@
 
 import Types
 
-struct FlushEvaluator {}
+/// `FlushEvaluator` provides methods to evaluate specific flushed hand types.
+/// Each method independently checks for a particular hand type without considering
+/// the overall strength of the hand in the context of NLHE poker hand rankings.
+///
+/// - Note: `FlushEvaluator` does not rank hand strengths.
+/// Therefore, it's possible for multiple methods to return `true` for the same hand.
+public struct FlushEvaluator {
+
+  /// A hand of five cards of the same suit, using zero, one, or two hole cards.
+  ///
+  /// - Parameters:
+  ///   - holeCards: The player's hole cards.
+  ///   - communityCards: The community cards.
+  ///
+  /// - Returns: True if the player has a flush, false otherwise.
+  public func hasFlush(
+    holeCards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    guard check(holeCards: holeCards, communityCards: communityCards) else { return false }
+    let histogram = (holeCards + communityCards).suitHistogram()
+    return histogram.values.contains(5)
+  }
+
+  // MARK: -
+
+  private func check(
+    holeCards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return holeCards.count == 2
+      && communityCards.count >= 3 && communityCards.count <= 5
+  }
+}
--- Tests/EvaluatorTests/FlushEvaluatorTests.swift
+++ Tests/EvaluatorTests/FlushEvaluatorTests.swift
@@ -12,4 +12,59 @@ import _PokerKitTestSupport
 
 final class FlushEvaluatorTests: PokerKitTestCase {
   private let sut = FlushEvaluator()
+
+  // MARK: Flush
+
+  func test_has_flush() {
+    let holeCards = [card(.ace, .hearts), card(.king, .hearts)]
+    let communityCards = [
+      card(.queen, .hearts),
+      card(.jack, .hearts),
+      card(.ten, .hearts),
+    ]
+    let hasFlush = sut.hasFlush(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasFlush)
+  }
+
+  func test_has_flush_extra_non_flush_cards() {
+    let holeCards = [card(.ace, .hearts), card(.king, .hearts)]
+    let communityCards = [
+      card(.queen, .hearts),
+      card(.jack, .hearts),
+      card(.ten, .hearts),
+      card(.nine, .diamonds),
+    ]
+    let hasFlush = sut.hasFlush(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasFlush)
+  }
+
+  func test_has_flush_in_community_cards() {
+    let holeCards = [card(.ace, .diamonds), card(.king, .diamonds)]
+    let communityCards = [
+      card(.queen, .hearts),
+      card(.jack, .hearts),
+      card(.ten, .hearts),
+      card(.nine, .hearts),
+      card(.eight, .hearts),
+    ]
+    let hasFlush = sut.hasFlush(holeCards: holeCards, communityCards: communityCards)
+    expectTrue(hasFlush)
+  }
+
+  func test_no_flush() {
+    let holeCards = [card(.ace, .hearts), card(.king, .diamonds)]
+    let communityCards = [
+      card(.queen, .hearts),
+      card(.jack, .hearts),
+      card(.ten, .hearts),
+      card(.nine, .diamonds),
+    ]
+    let hasFlush = sut.hasFlush(holeCards: holeCards, communityCards: communityCards)
+    expectFalse(hasFlush)
+  }
+
+  func test_no_flush_too_few_cards() {
+    let hasFlush = sut.hasFlush(holeCards: [], communityCards: [])
+    expectFalse(hasFlush)
+  }
 }
