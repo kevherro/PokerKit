6e9b0e26f9ec3a616bf0ee65bec6090b1b428a5c
Kevin Herro <kvherro@gmail.com>
2023-11-14 18:26:11 -0800

    pair evaluator stubs

--- Sources/Evaluators/HandEvaluators/PairEvaluator.swift
+++ Sources/Evaluators/HandEvaluators/PairEvaluator.swift
@@ -8,4 +8,86 @@
 
 import Types
 
-struct PairEvaluator {}
+struct PairEvaluator {
+
+  // MARK: Top Pair
+
+  func hasTopPair(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Top Pair Top Kicker
+
+  func hasTopPairTopKicker(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Second Pair
+
+  func hasSecondPair(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Overpair
+
+  func hasOverpair(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Two Pair
+
+  func hasTwoPair(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Set
+
+  func hasSet(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Three Of A Kind
+
+  func hasThreeOfAKind(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Four Of A Kind
+
+  func hasFourOfAKind(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+
+  // MARK: Full House
+
+  func hasFullHouse(
+    cards: [Card],
+    communityCards: [Card]
+  ) -> Bool {
+    return false
+  }
+}
