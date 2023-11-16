024fe5fc92d890f79a372cfbc54b36ba27c24745
Kevin Herro <kvherro@gmail.com>
2023-11-16 15:14:18 -0800

    types: street

---
+++ Sources/Types/Street.swift
@@ -0,0 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the PokerKit project.
+//
+// Copyright (c) 2023 Kevin Herro
+//
+//===----------------------------------------------------------------------===//
+
+public enum Street {
+  case flop, turn, river
+}
