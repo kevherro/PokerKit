23cdd743c7a9cf121f6b226839907e7db8adcd34
Kevin Herro <kvherro@gmail.com>
2023-11-21 17:09:49 -0800

    very scary min good hand tests

--- Tests/StrategiesTests/TightAggressive/MinGoodHandEvaluator/MinGoodHandEvaluatorTests+TwoPair.swift
+++ Tests/StrategiesTests/TightAggressive/MinGoodHandEvaluator/MinGoodHandEvaluatorTests+TwoPair.swift
@@ -12,4 +12,27 @@ import _PokerKitTestSupport
 
 final class MinGoodHandEvaluatorTests_TwoPair: PokerKitTestCase {
   private let sut = MinGoodHandEvaluator()
+
+  func testTwoPair_turn() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.ace, .diamonds),
+      card(.king, .hearts),
+      card(.king, .diamonds),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.fullHouse)
+  }
+
+  func testTwoPair_river() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.ace, .diamonds),
+      card(.king, .hearts),
+      card(.king, .diamonds),
+      card(.five, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.bestFullHouseUsingOneHoleCard)
+  }
 }
