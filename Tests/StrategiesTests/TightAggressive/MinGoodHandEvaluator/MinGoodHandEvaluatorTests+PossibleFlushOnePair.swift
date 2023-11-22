9648d701c7e13506a0f70be2784038bdad5075fd
Kevin Herro <kvherro@gmail.com>
2023-11-21 17:12:21 -0800

    very scary possible flush one pair min good hand tests

--- Tests/StrategiesTests/TightAggressive/MinGoodHandEvaluator/MinGoodHandEvaluatorTests+PossibleFlushOnePair.swift
+++ Tests/StrategiesTests/TightAggressive/MinGoodHandEvaluator/MinGoodHandEvaluatorTests+PossibleFlushOnePair.swift
@@ -12,4 +12,39 @@ import _PokerKitTestSupport
 
 final class MinGoodHandEvaluatorTests_PossibleFlushOnePair: PokerKitTestCase {
   private let sut = MinGoodHandEvaluator()
+
+  func testPossibleFlushOnePair_turn() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.ace, .diamonds),
+      card(.ten, .hearts),
+      card(.five, .hearts),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.nutFlush)
+  }
+
+  func testPossibleFlushOnePair_threeToFlush_river() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.ace, .diamonds),
+      card(.ten, .hearts),
+      card(.five, .hearts),
+      card(.two, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.fullHouse)
+  }
+
+  func testPossibleFlushOnePair_fourToFlush_river() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.ace, .diamonds),
+      card(.ten, .hearts),
+      card(.five, .hearts),
+      card(.two, .hearts),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.fullHouse)
+  }
 }
