be4cecedb577b64e0422bd5190d5c697feb15816
Kevin Herro <kvherro@gmail.com>
2023-11-21 16:57:51 -0800

    scary min good hand eval tests

--- Tests/StrategiesTests/MinGoodHandEvaluatorTests.swift
+++ Tests/StrategiesTests/MinGoodHandEvaluatorTests.swift
@@ -62,7 +62,7 @@ final class MinGoodHandEvaluatorTests: PokerKitTestCase {
 
   // MARK: Scary
 
-  func testScary_flop() {
+  func testScary_pair_flop() {
     let communityCards = [
       card(.king, .hearts),
       card(.king, .diamonds),
@@ -70,5 +70,105 @@ final class MinGoodHandEvaluatorTests: PokerKitTestCase {
     ]
     let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .flop)
     expectEqual(minGoodHand, MinGoodHand.topPair)
+  }
+
+  func testScary_threeToFlush_flop() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.eight, .hearts),
+      card(.five, .hearts),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .flop)
+    expectEqual(minGoodHand, MinGoodHand.topPair)
+  }
+
+  func testScary_threeToOpenEndedStraight_flop() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.queen, .diamonds),
+      card(.jack, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .flop)
+    expectEqual(minGoodHand, MinGoodHand.topPair)
+  }
+
+  func testScary_pair_turn() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.king, .diamonds),
+      card(.eight, .clubs),
+      card(.five, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.overpair)
+  }
+
+  func testScary_threeToFlush_turn() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.eight, .hearts),
+      card(.five, .hearts),
+      card(.two, .diamonds),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.overpair)
+  }
+
+  func testScary_threeToOpenEndedStraight_turn() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.queen, .diamonds),
+      card(.jack, .clubs),
+      card(.two, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.overpair)
+  }
+
+  func testScary_turn_aceHigh() {
+    let communityCards = [
+      card(.ace, .hearts),
+      card(.queen, .diamonds),
+      card(.queen, .hearts),
+      card(.two, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .turn)
+    expectEqual(minGoodHand, MinGoodHand.topPairTopKicker)
+  }
+
+  func testScary_pair_river() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.king, .diamonds),
+      card(.eight, .clubs),
+      card(.five, .clubs),
+      card(.four, .hearts),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.trips)
+  }
+
+  func testScary_threeToFlush_river() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.ten, .diamonds),
+      card(.eight, .hearts),
+      card(.five, .hearts),
+      card(.two, .diamonds),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.flush)
+  }
+
+  func testScary_threeToOpenEndedStraight_river() {
+    let communityCards = [
+      card(.king, .hearts),
+      card(.queen, .diamonds),
+      card(.jack, .clubs),
+      card(.five, .spades),
+      card(.two, .clubs),
+    ]
+    let minGoodHand = sut.minGoodHand(communityCards: communityCards, street: .river)
+    expectEqual(minGoodHand, MinGoodHand.straight)
   }
 }
